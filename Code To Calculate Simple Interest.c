217fc3c29b668a98c5861deab1a3d5393ec8cdcc
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-11-23 12:32:28 +0530

    Requesting Input For The Variables

--- Code To Calculate Simple Interest.c
+++ Code To Calculate Simple Interest.c
@@ -5,4 +5,10 @@
 void main()
 {
     float amount,rate,time,interest;
+    printf("Enter Principle Amount : ");
+    scanf("%f",&amount);
+    printf("Enter The Interest Rate : ");
+    scanf("%f",&rate);
+    printf("Enter Time In Year : ");
+    scanf("%f",&time);
 }
