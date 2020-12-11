16a7ff7a0708f79dbe3b3d711253a48059a3c3d9
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-12-11 14:57:37 +0530

    Requesting Input For Integer Variable 'b'

--- Code To Implement Nested IF ELSE.c
+++ Code To Implement Nested IF ELSE.c
@@ -11,5 +11,7 @@ void main()
     int a,b,c,s;
     printf("Please Enter A Value For The First Number : ");
     scanf("%d",&a);
+    printf("Please Enter A Value For The Second Number : ");
+    scanf("%d",&b);
 
 }
