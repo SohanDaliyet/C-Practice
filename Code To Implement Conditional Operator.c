4f0a17f662ba6eda3c5e75ee5380296044e50cfb
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-12-12 09:19:38 +0530

    Code To Implement Conditional Operator

---
+++ Code To Implement Conditional Operator.c
@@ -0,0 +1,11 @@
+/* C Code To Implement Conditional Operator */
+/* The Following Code Demonstrate The Greatest */
+/* Of Two Values Entered Through Keyboard. */
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+}
