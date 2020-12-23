65d972b7d522d025f56b780bdcdedad099cefd32
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-12-23 13:42:45 +0530

    Convert Fahrenheit To Celsius

---
+++ Code To Convert Fahrenheit To Celsius.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+void main()
+{
+    float fahrenheit, celsius;
+
+    printf("Enter Fahrenheit: ");
+
+    scanf("%f",&fahrenheit);
+
+    celsius = (fahrenheit - 32)*5/9;
+
+    printf("Celsius: %f \n", celsius);
+}
