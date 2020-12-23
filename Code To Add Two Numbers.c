aac0a052fd3833fe74646f90a019ff04f70d6a92
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-12-23 13:58:41 +0530

    Code To Add Two Numbers

---
+++ Code To Add Two Numbers.c
@@ -0,0 +1,19 @@
+/* C Code To Add Two Numbers */
+
+#include<stdio.h>
+#include<math.h>
+
+void main()
+{
+    int a,b,c;
+
+    printf("Please Input The First Number : ");
+    scanf("%d",&a);
+
+    printf("Please Input The Second Number : ");
+    scanf("%d",&b);
+
+    c=a+b;
+    printf("The Addition Of The Two Numbers Are : %d ",c);
+
+}
