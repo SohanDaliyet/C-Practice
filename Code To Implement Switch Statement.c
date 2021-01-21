79f504365b7ca590579511d9fa91d8101df007d2
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2021-01-21 11:44:14 +0530

    Implemented Switch Statement

--- Code To Implement Switch Statement.c
+++ Code To Implement Switch Statement.c
@@ -8,4 +8,15 @@ void main()
     int i;
     printf("Please Select A Number 1 Or 2 : ");
     scanf("d",&i);
+    switch(i)
+    {
+    case 1:
+        printf("I Am In Heaven !!!");
+        break;
+    case 2:
+        printf("I Am In Hell !!!");
+        break;
+    default:
+        printf("I Am Still On Earth !!!");
+    }
 }
