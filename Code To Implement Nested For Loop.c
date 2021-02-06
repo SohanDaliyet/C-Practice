d1359a4decdf5e91d0eb93c8b7ec72965603e3e3
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2021-02-06 11:22:05 +0530

    Implemented Nested For Loop

--- Code To Implement Nested For Loop.c
+++ Code To Implement Nested For Loop.c
@@ -5,5 +5,15 @@
 
 void main()
 {
+    int i,j;
+
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+        {
+            printf("\n The Value Of i = %d",i);
+            printf("\n The Value Of j = %d",j);
+        }
+    }
 
 }
