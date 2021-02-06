b851147580cc691d653b2b606c35cc10ade89ddb
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2021-02-06 10:27:31 +0530

    Implemented Do-While Statement

--- Code To Implement Do While Loop.c
+++ Code To Implement Do While Loop.c
@@ -7,4 +7,14 @@ void main()
 {
     int a,b,x;
     x = 1;
+
+    do{
+        printf("enter any two values : ");
+        scanf("%d%d",&a,&b);
+        a=a=b;
+        printf("The Addition Of Two Numbers Is %d ",a);
+        x++;
+    }while(x<=5);
+
+
 }
